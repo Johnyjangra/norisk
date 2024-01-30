135623eb4373b358e9630051dc475e7fa84ac905
Sumit chouhan <sumitkhan.dhana@gmail.com>
2024-01-30 21:14:30 +0530

    header updating

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// navbar
 function showNav() {
   var navBar = document.getElementById("navBar");
   var body = document.body;
@@ -9,3 +10,28 @@ function showNav() {
     ? "./assets/images/common/Cross.svg"
     : "./assets/images/common/nav-toggle.svg";
 }
+
+// video
+document.getElementById("playButton").addEventListener("click", function () {
+  var video = document.getElementById("myVideo");
+
+  if (video.paused) {
+    video.play();
+    this.style.display = "none";
+  } else {
+    video.pause();
+    this.style.display = "block";
+  }
+});
+
+document.getElementById("myVideo").addEventListener("click", function () {
+  var playButton = document.getElementById("playButton");
+
+  if (this.paused) {
+    this.play();
+    playButton.style.display = "none";
+  } else {
+    this.pause();
+    playButton.style.display = "block";
+  }
+});
