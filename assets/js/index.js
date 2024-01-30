d63c814b37d43737c721feb19f3b857671d083bf
Sumit chouhan <sumitkhan.dhana@gmail.com>
2024-01-30 20:10:32 +0530

    header done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+function showNav() {
+  var navBar = document.getElementById("navBar");
+  var body = document.body;
+  var navToggleIcon = document.getElementById("navToggleIcon");
+
+  navBar.classList.toggle("showNav");
+  body.classList.toggle("nav_mobile_scroll");
+  navToggleIcon.src = navBar.classList.contains("showNav")
+    ? "./assets/images/common/Cross.svg"
+    : "./assets/images/common/nav-toggle.svg";
+}
