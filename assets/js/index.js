41c1b7f394def8de2c3619801142c5ab069ab7fe
itzkunal01 <kunal.sharmag12@gmail.com>
2024-01-31 21:45:18 +0530

    updating index.html file

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,32 +6,32 @@ function showNav() {
 
   navBar.classList.toggle("show_nav");
   body.classList.toggle("nav_mobile_scroll");
-  navToggleIcon.src = navBar.classList.contains("show_nav")
-    ? "./assets/images/common/cross.svg"
-    : "./assets/images/common/nav-toggle.svg";
+  navToggleIcon.src = navBar.classList.contains("show_nav") ?
+    "./assets/images/common/cross.svg" :
+    "./assets/images/common/nav-toggle.svg";
 }
 
 // video
-document.getElementById("playButton").addEventListener("click", function () {
-  var video = document.getElementById("myVideo");
+// document.getElementById("playButton").addEventListener("click", function () {
+//   var video = document.getElementById("myVideo");
 
-  if (video.paused) {
-    video.play();
-    this.style.display = "none";
-  } else {
-    video.pause();
-    this.style.display = "block";
-  }
-});
+//   if (video.paused) {
+//     video.play();
+//     this.style.display = "none";
+//   } else {
+//     video.pause();
+//     this.style.display = "block";
+//   }
+// });
 
-document.getElementById("myVideo").addEventListener("click", function () {
-  var playButton = document.getElementById("playButton");
+// document.getElementById("myVideo").addEventListener("click", function () {
+//   var playButton = document.getElementById("playButton");
 
-  if (this.paused) {
-    this.play();
-    playButton.style.display = "none";
-  } else {
-    this.pause();
-    playButton.style.display = "block";
-  }
-});
+//   if (this.paused) {
+//     this.play();
+//     playButton.style.display = "none";
+//   } else {
+//     this.pause();
+//     playButton.style.display = "block";
+//   }
+// });
