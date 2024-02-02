4fb17f16c529bde35e0b1a0fa97e82dde4915065
itzkunal01 <kunal.sharmag12@gmail.com>
2024-02-02 09:52:56 +0530

    new page created livestreaming code

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,28 +10,3 @@ function showNav() {
     "./assets/images/common/cross.svg" :
     "./assets/images/common/nav-toggle.svg";
 }
-
-// VIDEO JS CODE
-document.getElementById("playButton").addEventListener("click", function () {
-  var video = document.getElementById("myVideo");
-
-  if (video.paused) {
-    video.play();
-    this.style.display = "none";
-  } else {
-    video.pause();
-    this.style.display = "block";
-  }
-});
-
-document.getElementById("myVideo").addEventListener("click", function () {
-  var playButton = document.getElementById("playButton");
-
-  if (this.paused) {
-    this.play();
-    playButton.style.display = "none";
-  } else {
-    this.pause();
-    playButton.style.display = "block";
-  }
-});
