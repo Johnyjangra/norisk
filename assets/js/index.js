181371952f681fde9d3888bae4e0f47b40faf3bf
surender singh <surendersingh9728511667@gmail.com>
2024-02-03 11:46:26 +0530

    back to top section

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,3 +28,23 @@ function showNav() {
       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
     });
   });
+
+
+  // ========== BACK TO TOP
+$(document).ready(function () {
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+          $("#scroll").fadeIn();
+      } else {
+          $("#scroll").fadeOut();
+      }
+  });
+  $("#scroll").click(function () {
+      $("html, body").animate({
+              scrollTop: 0,
+          },
+          40
+      );
+      return false;
+  });
+});
