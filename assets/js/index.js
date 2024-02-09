cc5b879ed3117fe69bd0c3a55e553e4982b8b603
Neha <neharadialcode@gmail.com>
2024-02-09 14:31:29 +0530

    scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,9 +6,9 @@ function showNav() {
 
   navBar.classList.toggle("show_nav");
   body.classList.toggle("nav_mobile_scroll");
-  navToggleIcon.src = navBar.classList.contains("show_nav") ?
-    "./assets/images/common/nav-cross.svg" :
-    "./assets/images/common/nav-toggle.svg";
+  navToggleIcon.src = navBar.classList.contains("show_nav")
+    ? "./assets/images/common/nav-cross.svg"
+    : "./assets/images/common/nav-toggle.svg";
 }
 // ========== BACK TO TOP JS CODE ==========
 $(document).ready(function () {
@@ -20,11 +20,31 @@ $(document).ready(function () {
     }
   });
   $("#scroll").click(function () {
-    $("html, body").animate({
+    $("html, body").animate(
+      {
         scrollTop: 0,
       },
       40
     );
     return false;
   });
+});
+
+const url = new URL(window.location);
+const sectionID = url.searchParams.get("section");
+const goTOSection = (id) => {
+  const section = document.querySelector(`#${id}`);
+
+  console.log(section);
+  section.scrollIntoView({
+    behavior: "smooth",
+  });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(() => {
+    if (sectionID) {
+      goTOSection(sectionID);
+    }
+  }, 1000);
 });
